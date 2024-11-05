20c8f20d8d6bb33c8a690e175ca8424fad81dfe6
hungmap0312 <hung.lt031202@gmail.com>
2024-11-05 16:20:36 +0700

    init project

--- main.cpp
+++ main.cpp
@@ -14,27 +14,76 @@ node *createNode(int x){
     return temp;
 }
 
-node *addElement(node*p, int x){
+void printList(node *l){
+	node *T = l;
+	while (T != NULL){
+		cout << T->data << " ";
+		T = T->next;
+	}
+}
+
+node *addElement(node*T, int x){
 	node *temp = createNode(x);
-	p->next = temp;
+	T->next = temp;
 	return temp;
 }
 
 node *addAt(node *l, int k, int x){
-	node *p = l;
-	for (int i = 0; i < k-1; i++){
-		p = p->next;
+	node *T = l;
+	for (int i = 0; i < k-2; i++){
+		T = T->next;
 	}
 	node *temp = new node;
 	temp->data = x;
-	temp->next = p->next;
-	p->next = temp;
+	temp->next = T->next;
+	T->next = temp;
 
+	return l;
+}
+
+node *deleteTail(node *l){
+	node *T = l;
+	while (T->next->next != NULL){
+		T = T->next;
+	}
+	delete(T->next);
+	T->next = NULL;
+	return l;
+}
+
+node *deleteAt(node *l, int k){
+	node *T = l;
+	for (int i = 0; i < k-2; i++){
+		T = T->next;
+	}
+	node *temp = T->next;
+	T->next = T->next->next;
+	delete(temp);
 	return l;
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int k;
+	node *l = createNode(1);
+	node *T = l;
+	for (int i = 2; i <= 10 ; i++){
+		T = addElement(T, i);
+	}
+	cout<<"Mang ban dau: ";
+	printList(l);
+
+    addAt(l, 6, 100);
+    cout<<"\nMang sau khi chen 100 vao phan tu thu 6: ";
+    printList(l);
+
+    deleteTail(l);
+    cout<<"\nMang sau khi xoa phan tu cuoi: ";
+    printList(l);
+
+    deleteAt(l, 6);
+    cout<<"\nMang sau khi xoa phan tu thu 6: ";
+    printList(l);
+
     return 0;
 }
